f4bb858462c5994bc67e9a4c1a910ad7c2e44f80
John MacKintosh <johnmackintosh@users.noreply.github.com>
2023-12-12 11:18:08 +0000

    Update day06.R

--- day06.R
+++ day06.R
@@ -1,28 +1,24 @@
+input <-  read.table("day06.txt")
 
-# test cases
-Time <- c(7, 15, 30)
-Distance <- c(9, 40, 200)
-
-# function
 ways_to_win <- function(Time = 7, Distance = 9) {
-  # no need to test 0 or max Distance
   t <- seq(from = 1, to = (Time - 1),  by = 1)
-  # reverse t, as the time remaining descends as time held increases
   d <- rev(t)
   res <- t * d
   res <- length(res[res > Distance])
 }
 
-# check
-res <- mapply(ways_to_win, Time, Distance) |> prod(res = _)
+input <-  read.table("day06.txt")
 
-# test with supplied data
-duration <- c(51, 92, 68, 90)
-distance <- c(222, 2031, 1126,  1225)
-res <- mapply(ways_to_win, duration, distance) |> prod(res = _)
+duration <- input[1,2:5]
+distance <- input[2,2:5]
+
+time <-  c(duration$V2,duration$V3,duration$V4, duration$V5)
+travel <- c(distance$V2,distance$V3,distance$V4, distance$V5)
+res <- mapply(ways_to_win, time, travel) |> prod(res = _)
 
 # part 2
-newtime <- 51926890
-newdist <- 222203111261225
+newtime <- as.numeric(paste0(duration$V2,duration$V3,duration$V4, duration$V5))
+newdist <- as.numeric(paste0(distance$V2,distance$V3,distance$V4, distance$V5))
 res2 <- ways_to_win(newtime, newdist)
-#gc()
+gc()
+
